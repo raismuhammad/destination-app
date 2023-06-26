0b47ca200f1d8ccde92fe204226087ada6f47426
raismuhammad <muhammadrais96@gmail.com>
2023-06-26 20:59:52 +0700

    add splashScreen

---
+++ app/src/main/java/com/raisproject/destinasi/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,33 @@
+package com.raisproject.destinasi.ui.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Window
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
+import com.raisproject.destinasi.R
+import com.raisproject.destinasi.databinding.ActivitySplashScreenBinding
+import com.raisproject.destinasi.ui.explore.ExploreActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen)
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.secondaryColor)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashScreenActivity, ExploreActivity::class.java))
+        },3000)
+    }
+}
